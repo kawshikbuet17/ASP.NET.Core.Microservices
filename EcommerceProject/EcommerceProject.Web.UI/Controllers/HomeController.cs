7295f03a73797f3d3b0a77f30b2f352792f01646
kawshikbuet17 <kawshikbuet17@gmail.com>
2024-01-01 17:01:08 +0600

    Designed Homepage

--- EcommerceProject/EcommerceProject.Web.UI/Controllers/HomeController.cs
+++ EcommerceProject/EcommerceProject.Web.UI/Controllers/HomeController.cs
@@ -1,23 +1,50 @@
 ﻿using EcommerceProject.Web.UI.Models;
+using EcommerceProject.Web.UI.Service.IService;
 using EcommerceProject.Web.UI.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Text.Json;
 
 namespace EcommerceProject.Web.UI.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IProductService _productService;
+        public HomeController(IProductService productService)
         {
-            _logger = logger;
+            _productService = productService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<ProductDto>? list = new();
+            ResponseDto? response = await _productService.GetAllProductsAsync();
+            if (response != null && response.IsSuccess == true)
+            {
+                list = JsonSerializer.Deserialize<List<ProductDto>>(Convert.ToString(response.Result), options: new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+            return View(list);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> ProductDetails(int productId)
+        {
+            ProductDto? model = new();
+            ResponseDto? response = await _productService.GetProductByIdAsync(productId);
+            if (response != null && response.IsSuccess == true)
+            {
+                model = JsonSerializer.Deserialize<ProductDto>(Convert.ToString(response.Result), options: new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+            return View(model);
         }
 
         [Authorize(Roles =SD.RoleAdmin)]
--- EcommerceProject/EcommerceProject.Web.UI/Models/ProductDto.cs
+++ EcommerceProject/EcommerceProject.Web.UI/Models/ProductDto.cs
@@ -1,4 +1,6 @@
-﻿namespace EcommerceProject.Web.UI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceProject.Web.UI.Models
 {
     public class ProductDto
     {
@@ -8,5 +10,9 @@
         public string Description { get; set; }
         public string CategoryName { get; set; }
         public string ImageUrl { get; set; }
+
+        [Range(1,100)]
+        public int Count { get; set; } = 1;
+
     }
 }
