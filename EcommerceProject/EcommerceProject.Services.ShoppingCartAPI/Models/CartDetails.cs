ed3da34863a52a0fa00c1f7570963ecff52449e0
kawshikbuet17 <kawshikbuet17@gmail.com>
2024-01-01 18:04:35 +0600

    ShoppingCartAPI created and Models added

---
+++ EcommerceProject/EcommerceProject.Services.ShoppingCartAPI/Models/CartDetails.cs
@@ -0,0 +1,19 @@
+﻿using EcommerceProject.Services.ShoppingCartAPI.Models.Dto;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcommerceProject.Services.ShoppingCartAPI.Models
+{
+    public class CartDetails
+    {
+        [Key]
+        public int CartDetailsId { get; set; }
+        public int CartHeaderId { get; set; }
+        [ForeignKey("CartHeaderId")]
+        public CartHeader CartHeader { get; set;}
+        public int ProductId { get; set; }
+        [NotMapped]
+        public ProductDto Product { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ EcommerceProject/EcommerceProject.Services.ShoppingCartAPI/Models/CartHeader.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcommerceProject.Services.ShoppingCartAPI.Models
+{
+    public class CartHeader
+    {
+        [Key]
+        int CartHeaderId { get; set; }
+        public string? UserId { get; set; }
+        public string? CouponCode { get; set; }
+        [NotMapped]
+        public double Discount { get; set; }
+        [NotMapped]
+        public double CartTotal { get; set; }
+    }
+}
